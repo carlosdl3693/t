f2b40ae783855045ffe27323c7c069bab80b5477
carlosdl3693 <carlos_dl3693@hotmail.com>
2022-02-10 12:13:54 -0500

    Update t.js

--- t.js
+++ t.js
@@ -1,7 +1,2 @@
-document.write('<body><iframe name="iFrameName" src="https://www.gruponatividad.com/" style="border:none;"></iframe></body>');
-window.onload = function(){
- setTimeout(function(){
-   document.getElementsByName('iFrameName')[0].src = 'https://www.gruponatividad.com/';
- },5000);
-};
+document.write('<body><iframe name="iFrameName" src="https://www.gruponatividad.com/" frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:150%;width:150%;position:absolute;top:0px;left:0px;right:0px;bottom:0px" height="150%" width="150%"></iframe></body>');
 
