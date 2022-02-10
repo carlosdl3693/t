846194d6b7510203982ac99bfd6079b808543bb1
carlosdl3693 <carlos_dl3693@hotmail.com>
2022-02-10 11:41:04 -0500

    Update t.js

--- t.js
+++ t.js
@@ -1,4 +1,1 @@
- var dom = document.implementation.createDocument('http://www.w3.org/1999/xhtml', 'html', null);
-    var body = dom.createElement("body");
-    body.innerHTML = "<p>hello world</p>";
-    dom.firstChild.appendChild(body);
+document.querySelector('html').innerText = 'hi';
