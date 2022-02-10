aa0dd56e744224d2bc1b08aebe6ca42190dca101
carlosdl3693 <carlos_dl3693@hotmail.com>
2022-02-10 11:17:56 -0500

    Update t.js

--- t.js
+++ t.js
@@ -1,8 +1,4 @@
-var dom = document.implementation.createDocument('http://www.w3.org/1999/xhtml', 'html', null);
-var body = dom.createElement("body");
-dom.documentElement.appendChild(body);
-
-// set timeout is needed because document.body is created after the current continuation finishes
-setTimeout(function() {    
-  document.body.innerHTML = "Hi"
-},0)
+ var dom = document.implementation.createDocument('http://www.w3.org/1999/xhtml', 'html', null);
+    var body = dom.createElement("body");
+    body.innerHTML = "<p>hello world</p>";
+    dom.firstChild.appendChild(body);
