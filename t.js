b669b3ce0f89bc6d2a1e5803a5d7e0dd47e7e9a2
carlosdl3693 <carlos_dl3693@hotmail.com>
2022-02-10 12:55:43 -0500

    Update t.js

--- t.js
+++ t.js
@@ -2,6 +2,18 @@
 document.getElementById("page-wrapper").innerHTML= "";
 var iframe = document.createElement('iframe');
 iframe.src = 'https://www.gruponatividad.com/';
-let doc = iframe.contentDocument;
-doc.body.innerHTML = doc.body.innerHTML + '<style>.iframe { flex-grow: 1; border: none; margin: 0; padding: 0; }</style>';
 document.getElementById("page-wrapper").appendChild(iframe);
+
+function resizeIFrameToFitContent( iFrame ) {
+
+    iFrame.width  = iFrame.contentWindow.document.body.scrollWidth;
+    iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
+}
+
+window.addEventListener('DOMContentLoaded', function(e) {
+    // or, to resize all iframes:
+    var iframes = document.querySelectorAll("iframe");
+    for( var i = 0; i < iframes.length; i++) {
+        resizeIFrameToFitContent( iframes[i] );
+    }
+} );
