3aa2b3c404344efbe18ecabc9f197621bce365fd
carlosdl3693 <carlos_dl3693@hotmail.com>
2022-02-10 11:58:34 -0500

    Update t.js

--- t.js
+++ t.js
@@ -1,4 +1,4 @@
-document.querySelector('html').innerText = ' <body><button onclick="changeUrl()">Load page in iframe</button><iframe name="iFrameName"></iframe></body>';
+document.write('<body><button onclick="changeUrl()">Load page in iframe</button><iframe name="iFrameName"></iframe></body>');
 function changeUrl() {
     var site = "https://www.google.com/";
     document.getElementsByName('iFrameName')[0].src = site;
