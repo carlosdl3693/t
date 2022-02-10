816dd21515c06a145aac3333a059e43b3c10c8b2
carlosdl3693 <carlos_dl3693@hotmail.com>
2022-02-10 12:31:11 -0500

    Update t.js

--- t.js
+++ t.js
@@ -1,6 +1,5 @@
 
 document.getElementById("page-wrapper").innerHTML= "";
 var iframe = document.createElement('iframe');
-var html = 'https://www.gruponatividad.com/';
-iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(html);
+iframe.src = 'https://www.gruponatividad.com/';
 document.getElementById("page-wrapper").appendChild(iframe);
